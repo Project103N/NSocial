6672e1797d08f2922160a3327bfb6f15bdd5d56a
Mehmet DEMİR <mail.mehmetdemir@gmail.com>
2022-02-07 20:09:25 +0300

    .

---
+++ Project103N/NSocial/Models/UserModels/Role.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace NSocial.Models.UserModels
+{
+    public class Role
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+    }
+}
---
+++ Project103N/NSocial/Models/UserModels/User.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace NSocial.Models.UserModels
+{
+    public class User
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+        public HttpPostedFileBase ProfileImage { get; set; }
+        public string ProfileImagePath { get; set; }
+        public string Phone { get; set; }
+        public int RoleID { get; set; }
+
+        public Role Role
+        {
+            get
+            {
+                return new Role();
+                //TODO: return RoleDAL.Methods.GetByID(this.RoleID);
+            }
+        }
+
+        public bool Login()
+        {
+            // TODO: Burada UserDAL kullanılarak oturum kontrolü yapılacak.
+            return true;
+        }
+
+    }
+}
