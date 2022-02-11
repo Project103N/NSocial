71ad6a2d7b010618f0e09f29b02f7a44c9f4204e
Birol Coruh <65693604+birolcoruh26@users.noreply.github.com>
2022-02-11 20:43:19 +0300

    post

--- Project103N/NSocial/Models/Post.cs
+++ Project103N/NSocial/Models/Post.cs
@@ -2,10 +2,24 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using System.IO;
+using NSocial.DataAccess;
+using NSocial.ModelBase;
+
 
 namespace NSocial.Models
 {
-    public class Post
-    {
-    }
+	public class Post
+	{
+		public int ID { get; set; }
+		public HttpPostedFileBase PostImagePath { get; set; }
+		public string Text { get; set; }
+		public DateTime PostDate { get; set; }
+		public int LikesCount { get; set; }
+		public int PunchsCount { get; set; }
+		public int CommentsCount { get; set; }
+		public int UserID { get; set; }
+		public User RoleID { get; set; }
+		public ICollection<Post> Comments { get; set; }
+	}
 }
