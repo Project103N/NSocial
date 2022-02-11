c32eaf03a6d3999abc7146933badfc85ee2fc1b1
Sena Arıcan <sena.arican99@gmail.com>
2022-02-11 20:18:27 +0300

    Like/Dislike

---
+++ Project103N/NSocial/Models/Reaction.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace NSocial.Models
+{
+    public class Reaction
+    {
+        public int ID { get; set; }
+        public int PostID { get; set; }
+        public int UserID { get; set; }
+        public int ReactionType { get; set; }
+    }
+}
